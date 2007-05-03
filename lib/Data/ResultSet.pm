4586f4ddeb13cbd79e447070ed21f13fca185911
Dave O'Neill <dmo@roaringpenguin.com>
2007-05-03 15:00:13 +0000

    Actual implementation of Data::ResultSet, with tests.
    r6534@oxygen:  dmo | 2007-05-03 11:01:50 -0400

--- lib/Data/ResultSet.pm
+++ lib/Data/ResultSet.pm
@@ -4,6 +4,134 @@ use strict;
 
 our $VERSION = '0.001';
 
+sub new
+{
+	my ($class) = @_;
+	return bless [], $class;
+}
+
+sub make_wrappers
+{
+	my ($class, @methods ) = @_;
+
+	$class->make_wrappers_for_all(@methods);
+	$class->make_wrappers_for_has(@methods);
+	$class->make_wrappers_for_get(@methods);
+	$class->make_wrappers_for_get_not(@methods);
+
+	return;
+}
+
+sub make_wrappers_for_get
+{
+	my ($class, @methods) = @_;
+
+	my $generator = sub {
+		my ($methodname) = @_;
+		return sub {
+			return grep { $_->$methodname() } @{$_[0]};
+		};
+	};
+
+	return $class->_generate_methods( 'get', $generator, @methods );
+}
+
+sub make_wrappers_for_get_not
+{
+	my ($class, @methods) = @_;
+
+	my $generator = sub {
+		my ($methodname) = @_;
+		return sub {
+			return grep { ! $_->$methodname() } @{$_[0]};
+		};
+	};
+
+	return $class->_generate_methods( 'get_not', $generator, @methods );
+}
+
+sub make_wrappers_for_has
+{
+	my ($class, @methods) = @_;
+
+	my $generator = sub {
+		my ($methodname) = @_;
+		return sub {
+			for( @{$_[0]} ) {
+				if( $_->$methodname() ) {
+					return 1;
+				}
+			}
+			return 0;
+		};
+	};
+
+	return $class->_generate_methods( 'has', $generator, @methods );
+}
+
+sub make_wrappers_for_all
+{
+	my ($class, @methods) = @_;
+
+	my $generator = sub {
+		my ($methodname) = @_;
+		return sub {
+			for( @{$_[0]} ) {
+				if( ! $_->$methodname() ) {
+					return 0;
+				}
+			}
+			return 1;
+		};
+	};
+
+	return $class->_generate_methods( 'all', $generator, @methods );
+}
+
+sub _generate_methods
+{
+	my ($class, $prefix, $generator, @methods) = @_; 
+
+	no strict 'refs';  ## no critic (ProhibitNoStrict)
+	foreach my $name (@methods) {
+		my $wrappername = $name;
+		$wrappername =~ s/is_//;
+		$wrappername = "${class}::${prefix}_${wrappername}";
+		if( ! defined &{$wrappername} ) {
+			*{$wrappername} = $generator->($name);
+
+		}
+	}
+
+	return;
+}
+
+sub add
+{
+	my ($self, $obj) = @_;
+	push @{$self}, $obj;
+	return $self;
+}
+
+sub clear
+{
+	my ($self) = @_;
+	@{$self} = ();
+	return 1;
+}
+
+sub count
+{
+	my ($self) = @_; 
+	return scalar @{$self};
+}
+
+sub contents
+{
+	my ($self) = @_;
+	return @{$self};
+}
+
 1;
 __END__
 
@@ -17,8 +145,8 @@ Data::ResultSet - Container for aggregating and examining multiple results
     package MyApp::ResultSet;
     use base qw( Data::ResultSet );
 
-    # Generate methods to wrap 'is_success'
-    __PACKAGE__->generate_wrappers( qw( is_success is_error ) );
+    # Generate methods to wrap 'is_success' and 'is_error'
+    __PACKAGE__->make_wrappers( qw( is_success is_error ) );
 
     # And elsewhere...
     package MyApp;
@@ -63,7 +191,18 @@ method signature to be returned as a single object that can then be
 queried for success or failure in a number of ways.
 
 This is accomplished by generating wrappers to methods in the
-underlying list of result objects.  For example, if 
+underlying list of result objects.  For example, if you have a result
+object that has an is_ok() method, you can create a Data::ResultSet
+subclass to handle it with:
+
+    package MyApp::ResultSet;
+    use base qw( Data::ResultSet );
+    __PACKAGE__->make_wrappers( 'is_ok' );
+    1;
+
+This will generate C<all_ok>, C<has_ok>, C<get_ok>, and C<get_not_ok>
+methods in MyApp::ResultSet that use the C<is_ok> accessor on your
+result object.
 
 =head1 CLASS METHODS
 
@@ -72,7 +211,7 @@ underlying list of result objects.  For example, if
 Creates a new Data::ResultSet object.  Generally you will want to do
 this on a subclass, not on Data::ResultSet.
 
-=head2 generate_wrappers ( @method_names )
+=head2 make_wrappers ( @method_names )
 
 Generates all wrapper methods ( all_, has_, get_, get_not ) for the
 provided method names.  The resulting wrapper will consist of the
@@ -81,27 +220,39 @@ provided names beginning with is_ will have the is_ stripped first.
 
 The wrappers can be generated individually using other methods (see below).
 
-=head2 generate_all_wrappers
+=head2 make_wrappers_for_all ( @method_names )
 
-TODO
+Generates the C<all_> wrapper method for each provided name.
 
-=head2 generate_has_wrappers
+=head2 make_wrappers_for_has
 
-TODO
+Generates the C<has_> wrapper method for each provided name.
 
-=head2 generate_get_wrappers
+=head2 make_wrappers_for_get
 
-TODO
+Generates the C<get_> wrapper method for each provided name.
 
-=head2 generate_get_not_wrappers
+=head2 make_wrappers_for_get_not
 
-TODO
+Generates the C<get_not_> wrapper method for each provided name.
 
 =head1 INSTANCE METHODS
 
 =head2 add ( $object ) 
 
-Adds an object to the result set.
+Adds an object to the result set.  Returns $self.
+
+=head2 count ( ) 
+
+Returns number of objects in the set.
+
+=head2 contents ( )
+
+Returns contents of set.
+
+=head2 clear ( ) 
+
+Clears contents of set.  Returns true.
 
 =head2 all_METHOD ( )
 
@@ -126,12 +277,19 @@ Generated method that returns all objects for which METHOD returns false.
 There are no known incompatibilities with this module.
  
 =head1 BUGS AND LIMITATIONS
+
+=over 4
+
+=item *
+
+The methods being wrapped shouldn't be anything more than simple
+accessors.  They will get called an arbitrary number of times, so doing
+any real work, particularly anything that changes state or has
+side-effects, is a bad idea.
+
+=back
  
-There are no known bugs in this module. 
-
-Please report problems to the author.
-
-Patches are welcome.
+Please report any new problems to the author.  Patches are welcome.
  
 =head1 AUTHOR
  
